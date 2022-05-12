fa8039535b4bdfc16985c557cd55475a9382c734
Kenneth Porterfield <kjport3@gmail.com>
2022-05-12 12:20:24 -0400

    Add Padding to Homepage Img

--- components/layout.js
+++ components/layout.js
@@ -50,6 +50,7 @@ export default function Layout({ children, home }) {
               className={utilStyles.borderCircle}
               height={144}
               width={144}
+              padding={50}
               alt={name}
             />
             <h1 className={utilStyles.heading2Xl}>{name}</h1>
